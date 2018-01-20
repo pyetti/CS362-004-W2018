b216a887b510b4e7b49435bc0c66a6ebe53a9eb5
Peter Yetti <pyetti@users.noreply.github.com>
2018-01-20 18:27:27 -0500

    removed bug that introduced infinite loop

--- projects/yettip/dominion/cards.c
+++ projects/yettip/dominion/cards.c
@@ -11,28 +11,31 @@ Description: Implementation for Card.hpp
 void smithyCard(int currentPlayer, int handPos, struct gameState *state) {
 	//+3 Cards
 	int i;
-	for (i = 0; i < 3; i++) {
+	// TODO introduced bug: added <= instead of <
+	for (i = 0; i <= 3; i++) {
 		drawCard(currentPlayer, state);
 	}
 
 	//discard card from hand
-	// TODO remember that the bug is here (reversed handPos and currentPlayer)
+	// TODO introduced bug: reversed handPos and currentPlayer
 	discardCard(currentPlayer, handPos, state, 0);
 }
 
 void adventurerCard(int currentPlayer, struct gameState *state) {
 	int drawnTreasure = 0;
 	int cardDrawn;
-	int counterForTempHand = 0;// this is the counter for the temp hand
+	int counterForTempHand = 0;
 	int tempHand[MAX_HAND];// moved above the if statement
 
-	while (drawnTreasure < 2) {
+	// TODO introduced bug: increased Treasure size for Adventurer from 2 to 4
+	while (drawnTreasure < 4) {
 		if (state->deckCount[currentPlayer] < 1) {//if the deck is empty we need to shuffle discard and add to deck
 			shuffle(currentPlayer, state);
 		}
 		drawCard(currentPlayer, state);
 		cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer] - 1];//top card of hand is most recently drawn card.
-		if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+		// TODO introduced bug: not checking for silver or gold card
+		if (cardDrawn == copper /**|| cardDrawn == silver || cardDrawn == gold**/)
 			drawnTreasure++;
 		else {
 			tempHand[counterForTempHand] = cardDrawn;
@@ -48,21 +51,23 @@ void adventurerCard(int currentPlayer, struct gameState *state) {
 
 void baronCard(int currentPlayer, int choice1, struct gameState *state) {
 	state->numBuys++;//Increase buys by 1!
+
 	if (choice1 > 0) {//Boolean true or going to discard an estate
-		int p = 0;//Iterator for hand!
+		int iteratorForHand = 0;
 		int card_not_discarded = 1;//Flag for discard set!
+
 		while (card_not_discarded) {
-			if (state->hand[currentPlayer][p] == estate) {//Found an estate card!
+			if (state->hand[currentPlayer][iteratorForHand] == estate) {//Found an estate card!
 				state->coins += 4;//Add 4 coins to the amount of coins
-				state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][p];
+				state->discard[currentPlayer][state->discardCount[currentPlayer]] = state->hand[currentPlayer][iteratorForHand];
 				state->discardCount[currentPlayer]++;
-				for (; p < state->handCount[currentPlayer]; p++) {
-					state->hand[currentPlayer][p] = state->hand[currentPlayer][p + 1];
+				for (; iteratorForHand < state->handCount[currentPlayer]; iteratorForHand++) {
+					state->hand[currentPlayer][iteratorForHand] = state->hand[currentPlayer][iteratorForHand + 1];
 				}
 				state->hand[currentPlayer][state->handCount[currentPlayer]] = -1;
 				state->handCount[currentPlayer]--;
 				card_not_discarded = 0;//Exit the loop
-			} else if (p > state->handCount[currentPlayer]) {
+			} else if (iteratorForHand > state->handCount[currentPlayer]) {
 				if (DEBUG) {
 					printf("No estate cards in your hand, invalid choice\n");
 					printf("Must gain an estate if there are any\n");
@@ -76,16 +81,17 @@ void baronCard(int currentPlayer, int choice1, struct gameState *state) {
 				}
 				card_not_discarded = 0;//Exit the loop
 			} else {
-				p++;//Next card
+				iteratorForHand++;//Next card
 			}
 		}
 	} else {
 		if (supplyCount(estate, state) > 0) {
 			gainCard(estate, state, 0, currentPlayer);//Gain an estate
 			state->supplyCount[estate]--;//Decrement Estates
-			if (supplyCount(estate, state) == 0) {
-				isGameOver(state);
-			}
+			// TODO introduced bug: not checking if game is over after discarding an estate
+//			if (supplyCount(estate, state) == 0) {
+//				isGameOver(state);
+//			}
 		}
 	}
 }
@@ -95,14 +101,13 @@ void minionCard(int currentPlayer, int handPos, int choice1, int choice2, struct
 	state->numActions++;
 	int i, j;
 
+	// TODO introduced bug: commented out discardCard from hand
 	//discard card from hand
-	discardCard(handPos, currentPlayer, state, 0);
+//	discardCard(handPos, currentPlayer, state, 0);
 
-	if (choice1)        //+2 coins
-	{
+	if (choice1) { //+2 coins
 		state->coins = state->coins + 2;
-	} else if (choice2)        //discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-	{
+	} else if (choice2) { //discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
 		//discard hand
 		while (numHandCards(state) > 0) {
 			discardCard(handPos, currentPlayer, state, 0);
@@ -115,7 +120,8 @@ void minionCard(int currentPlayer, int handPos, int choice1, int choice2, struct
 
 		//other players discard hand and redraw if hand size > 4
 		for (i = 0; i < state->numPlayers; i++) {
-			if (i != currentPlayer) {
+			// TODO introduced bug: not checking for current player before discarding and redrawing cards
+//			if (i != currentPlayer) {
 				if (state->handCount[i] > 4) {
 					//discard hand
 					while (state->handCount[i] > 0) {
@@ -128,7 +134,7 @@ void minionCard(int currentPlayer, int handPos, int choice1, int choice2, struct
 					}
 				}
 			}
-		}
+//		}
 
 	}
 }
