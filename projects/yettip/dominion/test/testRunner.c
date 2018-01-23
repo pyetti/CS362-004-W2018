bf949514cad0c31163753e49c3ed8f298e25c7fe
Peter Yetti <pyetti@users.noreply.github.com>
2018-01-23 01:41:33 -0500

    Finised unittest2; tested buyCard

--- projects/yettip/dominion/test/testRunner.c
+++ projects/yettip/dominion/test/testRunner.c
@@ -4,18 +4,17 @@ Date:  1/22/18
 Description: Implementation for testRunner.c
 *************************************************************************/
 
-#include <stdio.h>
 #include "unittest.h"
 
 int main() {
 
 	struct unittest test1 = {unittest1};
-//	struct unittest test2 = {unittest2};
-//	struct unittest test3 = {unittest3};
-//	struct unittest test4 = {unittest4};
+	struct unittest test2 = {unittest2};
+	struct unittest test3 = {unittest3};
+	struct unittest test4 = {unittest4};
 
 	runner(&test1);
-//	runner(&test2);
-//	runner(&test3);
-//	runner(&test4);
+	runner(&test2);
+	runner(&test3);
+	runner(&test4);
 }
--- projects/yettip/dominion/test/unittest2.c
+++ projects/yettip/dominion/test/unittest2.c
@@ -4,10 +4,43 @@ Date:  1/22/18
 Description: Implementation for unittest2.c
 *************************************************************************/
 
+#include "../dominion.h"
 #import "unittest.h"
+#include "assert.h"
+
+void test_buy_card_fails(struct gameState);
+void test_buy_card_passes(struct gameState);
 
 static const int run(void) {
+	struct gameState G;
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+	initializeGame(2, k, 2, &G);
+
+	test_buy_card_fails(G);
+	test_buy_card_passes(G);
 	return 2;
+}
+
+void test_buy_card_fails(struct gameState G) {
+	G.numBuys = 0;
+	assertA_Equals_B(-1, buyCard(province, &G), "User has no buys");
+
+	G.numBuys = 1;
+	assertA_Equals_B(-1, buyCard(province, &G), "User doesn't have enough coins");
+
+	assertA_Equals_B(-1, buyCard(baron, &G), "No Baron Cards to buy");
+}
+
+void test_buy_card_passes(struct gameState G) {
+	int currentNumberOfBuys = G.numBuys;
+	int player = G.whoseTurn;
+	int discardCount = G.discardCount[player];
+	assertA_Equals_B(10, G.supplyCount[smithy], "Player starts with 10 smithy cards");
+	assertA_Equals_B(0, buyCard(smithy, &G), "Player buys smithy");
+	assertA_Equals_B(9, G.supplyCount[smithy], "Player now has 9 smithy cards");
+	assertA_Equals_B(currentNumberOfBuys - 1, G.numBuys, "Player has 1 less number of buys");
+	assertA_Equals_B(0, G.coins, "Player now has 9 smithy cards");
+	assertA_Equals_B(discardCount + 1, G.discardCount[player], "Player has 1 more card in discard deck");
 }
 
 const struct unittest_vtable_ unittest2[] = { { run } };
