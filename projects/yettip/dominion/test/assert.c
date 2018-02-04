88c72595380971c7a97f98fefa33457206c4e90a
Peter Yetti <pyetti@users.noreply.github.com>
2018-02-04 01:56:04 -0500

    updated tests to print expected and actual values using generic macro

--- projects/yettip/dominion/test/assert.c
+++ projects/yettip/dominion/test/assert.c
@@ -7,20 +7,54 @@ Description: Implementation for assert.c
 #include <stdio.h>
 #include "assert.h"
 
+void print_int(int x) {
+	printf("%d", x);
+}
+
+void print_double(double x) {
+	printf("%f", x);
+}
+
+void print_string(char *x) {
+	printf("%s", x);
+}
+
+void print_char(char x) {
+	printf("%c", x);
+}
+
+#define print(X) _Generic((X),             \
+                     float: print_double,  \
+                     double: print_double, \
+                     default: print_int    \
+                 )(X)
+
 void assertA_Equals_B(const void *expected, const void *actual, const char* testName) {
+	char *testResult;
 	if (expected == actual) {
-		printf("TEST PASSED: %s\n", testName);
+		testResult = "PASSED";
 	} else {
-		printf("TEST FAILED: %s\n", testName);
+		testResult = "FAILED";
 	}
+	printf("TEST %s: %s. Expected: ", testResult ,testName);
+	print(expected);
+	printf(", Actual: ");
+	print(actual);
+	printf("\n");
 }
 
 void assertA_GreaterThan_B(const void *expected, const void *actual, const char* testName) {
+	char *testResult;
 	if (expected > actual) {
 		printf("TEST PASSED: %s\n", testName);
 	} else {
 		printf("TEST FAILED: %s\n", testName);
 	}
+	printf("TEST %s: %s. Expected: ", testResult ,testName);
+	print(expected);
+	printf(", Actual: ");
+	print(actual);
+	printf("\n");
 }
 
 void assertTrue(const void *value, const char* testName) {
