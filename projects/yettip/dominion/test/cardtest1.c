db6ed94cc99c5506b45cbbfa40727e32b2eee4aa
Peter Yetti <pyetti@users.noreply.github.com>
2018-02-04 02:43:46 -0500

    Tested Adventurer card

--- projects/yettip/dominion/test/cardtest1.c
+++ projects/yettip/dominion/test/cardtest1.c
@@ -4,6 +4,7 @@ Date:  1/28/18
 Description: Implementation for cardtest1.c
 *************************************************************************/
 
+#include <stdio.h>
 #include "../dominion.h"
 #include "../dominion_helpers.h"
 #import "unittest.h"
@@ -16,7 +17,7 @@ static const int run(void) {
 	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
 	initializeGame(2, k, 2, &G);
 	test_smithy_card(G);
-	return 4;
+	return 1;
 }
 
 void test_smithy_card(struct gameState gameState) {
@@ -24,6 +25,7 @@ void test_smithy_card(struct gameState gameState) {
 	cardEffect(smithy, 1, 0, 0, &gameState, 0, 0);
 	assertA_Equals_B(7, gameState.handCount[whoseTurn], "Hand count from cardEffect(smithy)");
 	assertA_Equals_B(2, gameState.deckCount[whoseTurn], "Deck count from cardEffect(smithy)");
+	printf("\n");
 }
 
 const struct unittest_vtable_ card_unittest1[] = {{run}};
--- projects/yettip/dominion/test/cardtest2.c
+++ projects/yettip/dominion/test/cardtest2.c
@@ -4,13 +4,28 @@ Date:  1/28/18
 Description: Implementation for cardtest2.c
 *************************************************************************/
 
+#include <stdio.h>
 #include "../dominion.h"
+#include "../dominion_helpers.h"
 #import "unittest.h"
 #include "assert.h"
 
-static const int run(void) {
+void test_adventurer_card(struct gameState gameState);
 
-	return 4;
+static const int run(void) {
+	struct gameState G;
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
+	initializeGame(2, k, 2, &G);
+	test_adventurer_card(G);
+	return 2;
+}
+
+void test_adventurer_card(struct gameState gameState) {
+	int whoseTurn = gameState.whoseTurn;
+	cardEffect(adventurer, 0, 0, 0, &gameState, 0, 0);
+	assertA_Equals_B(7, gameState.handCount[whoseTurn], "Hand count from cardEffect(adventurer)");
+	assertA_Equals_B(3, gameState.deckCount[whoseTurn], "Deck count from cardEffect(adventurer)");
+	printf("\n");
 }
 
 const struct unittest_vtable_ card_unittest2[] = {{run}};
