3982483516ba532cfb70faec37b4f182a5bcb87d
Peter Yetti <pyetti@users.noreply.github.com>
2018-02-14 02:27:45 -0500

    Might have finished Adventurer Random Testing

--- projects/yettip/dominion/test/randomTestRunner.c
+++ projects/yettip/dominion/test/randomTestRunner.c
@@ -8,11 +8,11 @@ Description: Implementation for randomTestRunner.c
 
 int main(int argc, char **argv) {
 	struct unittest adventurer = {randomtestcard1};
-	struct unittest card2 = {randomtestcard2};
-	struct unittest card3 = {randomtestcard3};
+//	struct unittest card2 = {randomtestcard2};
+//	struct unittest card3 = {randomtestcard3};
 
 	runner(&adventurer);
-	runner(&card2);
-	runner(&card3);
+//	runner(&card2);
+//	runner(&card3);
 	return 1;
 }
--- projects/yettip/dominion/test/randomtestcard1.c
+++ projects/yettip/dominion/test/randomtestcard1.c
@@ -4,24 +4,37 @@ Date:  2/13/18
 Description: Implementation for randomTester.c
 *************************************************************************/
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../dominion.h"
-#include "../dominion_helpers.h"
 #include "unittest.h"
+#include "TestUtility.h"
 #include "assert.h"
+#include "../cards.h"
 
-void testAdventurer(int numberOfCards, int numberOfPlayers);
+void testAdventurer();
 
 static const int run(void) {
-	testAdventurer(10, 2);
+	srand(time(NULL));
+	testAdventurer();
 	return 9;
 }
 
-void testAdventurer(int numberOfCards, int numberOfPlayers) {
-	struct gameState G;
-//	int k = (int) malloc(sizeof(int) * numberOfCards);
-	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy};
-	initializeGame(2, k, 2, &G);
+void testAdventurer() {
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, baron, sea_hag, tribute, smithy};
+	struct gameState state;
+	for (int test = 0; test < 1000; ++test) {
+		int numOfPlayers = rand() % (4 + 1 - 2) + 2;
+		initializeGame(numOfPlayers, k, 2, &state);
+		int player = rand() % (numOfPlayers);
+		int coinsBefore = cardCountInHand(player, copper, state) + cardCountInHand(player, silver, state) + cardCountInHand(player, gold, state);
+		adventurerCard(player, &state);
+		int coinsAfter = cardCountInHand(player, copper, state) + cardCountInHand(player, silver, state) + cardCountInHand(player, gold, state);
+		printf("TEST ADVENTURER CARD: coins before = %d, coins after = %d | ", coinsBefore, coinsAfter);
+		assertA_GreaterThan_B((void*) coinsAfter, (void*) coinsBefore + 1, "");
+	}
+	printf("\nDONE");
 }
 
 const struct unittest_vtable_ randomtestcard1[] = {{run}};
