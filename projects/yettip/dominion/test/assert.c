1e3e004aeffabb86ed50d41b9718ff007563046e
Peter Yetti <pyetti@users.noreply.github.com>
2018-02-14 02:27:26 -0500

    added another assert

--- projects/yettip/dominion/test/assert.c
+++ projects/yettip/dominion/test/assert.c
@@ -7,54 +7,28 @@ Description: Implementation for assert.c
 #include <stdio.h>
 #include "assert.h"
 
-//void print_int(int x) {
-//	printf("%d", x);
-//}
-//
-//void print_double(double x) {
-//	printf("%f", x);
-//}
-//
-//void print_string(char *x) {
-//	printf("%s", x);
-//}
-//
-//void print_char(char x) {
-//	printf("%c", x);
-//}
-//
-//#define print(X) _Generic((X),             \
-//                     float: print_double,  \
-//                     double: print_double, \
-//                     default: print_int    \
-//                 )(X)
-
 void assertA_Equals_B(const void *expected, const void *actual, const char* testName) {
-	char *testResult;
 	if (expected == actual) {
-		testResult = "PASSED";
+		printf("TEST PASSED: %s\n", testName);
 	} else {
-		testResult = "FAILED";
+		printf("TEST FAILED: %s\n", testName);
 	}
-	printf("TEST %s: %s.\n", testResult ,testName);
-//	print(expected);
-//	printf(", Actual: ");
-//	print(actual);
-//	printf("\n");
 }
 
 void assertA_GreaterThan_B(const void *expected, const void *actual, const char* testName) {
-	char *testResult;
 	if (expected > actual) {
 		printf("TEST PASSED: %s\n", testName);
 	} else {
 		printf("TEST FAILED: %s\n", testName);
 	}
-	printf("TEST %s: %s.\n", testResult ,testName);
-//	print(expected);
-//	printf(", Actual: ");
-//	print(actual);
-//	printf("\n");
+}
+
+void assertA_GreaterThanOrEqualTo_B(const void *expected, const void *actual, const char* testName) {
+	if (expected >= actual) {
+		printf("TEST PASSED: %s\n", testName);
+	} else {
+		printf("TEST FAILED: %s\n", testName);
+	}
 }
 
 void assertTrue(const void *value, const char* testName) {
